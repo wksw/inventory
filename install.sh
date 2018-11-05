82ebf165c8fd7b36536559de0da7674f3f48af6f
wksw <2223721904@qq.com>
2018-11-06 01:19:33 +0800

    add log

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 ROOTDIR=$(cd $(dirname $0);pwd)
 
 cat << EOF > $ROOTDIR/crontab
-30 23 * * * /bin/bash $ROOTDIR/get_parse.sh
+30 23 * * * /bin/bash $ROOTDIR/get_parse.sh >> $ROOTDIR/parse.log
 EOF
 
 crontab $ROOTDIR/crontab
