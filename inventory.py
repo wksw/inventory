1be6df3b42a9c9da7608c58da5fc51aa4c6a7a8a
wksw <2223721904@qq.com>
2018-11-07 23:41:16 +0800

    change style

--- inventory.py
+++ inventory.py
@@ -41,8 +41,9 @@ def write_output(output, output1, filename):
 	sheet2 = wb.add_sheet("采购单", cell_overwrite_ok = True)
 	# 设置列宽
 	sheet1.col(0).width = 3333 * 1
-	sheet1.col(1).width = 3333 * 2
+	sheet1.col(1).width = 3333 * 3
 	sheet1.col(2).width = 4200
+	sheet1.col(2).height = 3333
 	sheet1.col(3).width = 1666
 	sheet1.col(4).width = 2000
 	sheet1.col(5).width = 2000
@@ -50,15 +51,18 @@ def write_output(output, output1, filename):
 	sheet1.col(7).width = 2000
 	sheet1.col(8).width = 3333 * 1
 	sheet1.col(9).width = 3333
-	sheet1.col(10).width = 3333 * 4
+	sheet1.col(10).width = 3333 * 3
 
-	sheet2.col(0).width = 3333 * 2
-	sheet2.col(1).width = 4200
-	sheet2.col(2).width = 1666
-	sheet2.col(3).width = 2666
-	sheet2.col(4).width = 1666
-	sheet2.col(5).width = 2666
-	sheet2.col(6).width = 3333
+	sheet2.col(0).width = 1500
+	sheet2.col(1).width = 3333 * 3
+	sheet2.col(2).width = 4200
+	sheet2.col(3).width = 1666
+	sheet2.col(4).width = 2666
+	sheet2.col(5).width = 1666
+	sheet2.col(6).width = 2666
+	sheet2.col(7).width = 3333
+	sheet2.col(8).width = 3333
+
 
 	# 头部颜色，边框
 	pattern = xlwt.Pattern()
@@ -80,6 +84,7 @@ def write_output(output, output1, filename):
 	alignment = xlwt.Alignment()
 	alignment.horz = xlwt.Alignment.HORZ_CENTER
 	alignment.vert = xlwt.Alignment.VERT_CENTER
+	alignment.wrap = xlwt.Alignment.WRAP_AT_RIGHT
 
 	# 头部样式
 	header_style = xlwt.XFStyle()
@@ -90,7 +95,7 @@ def write_output(output, output1, filename):
 	# 标题字体
 	font_title = xlwt.Font()
 	font_title.bold = True
-	font_title.height = 240
+	font_title.height = 12 * 20
 
 
 	# 标题样式
@@ -101,6 +106,7 @@ def write_output(output, output1, filename):
 
 	center_style = xlwt.XFStyle()
 	center_style.alignment = alignment
+	center_style.borders = borders
 
 	# 颜色区分
 	pattern1 = xlwt.Pattern()
@@ -129,6 +135,9 @@ def write_output(output, output1, filename):
 		# 头部内容写入
 		for index in range(0, len(header)):
 			sheet1.write(key_index + goods_list_len + 2, index, header[index], header_style)
+		# 设置头部高度
+		sheet1.row(key_index + goods_list_len + 2).height_mismatch = True
+		sheet1.row(key_index + goods_list_len + 2).height = 256 * 3
 		# 商品
 		for goods_index in range(0, len(output[key]["goods_list"])):
 			# 商品名
@@ -137,9 +146,13 @@ def write_output(output, output1, filename):
 			sheet1.write(goods_index + key_index+goods_list_len + 3, 2, output[key]["goods_list"][goods_index]["flavor"].decode("gbk"), center_style)
 			# 商品数量
 			sheet1.write(goods_index + key_index+goods_list_len + 3, 3, output[key]["goods_list"][goods_index]["number"].decode("gbk"), center_style)
+			for e_i in range(4,9):
+				sheet1.write(goods_index + key_index+goods_list_len + 3, e_i, "", center_style)
 
 		# 店主
 		sheet1.write_merge(key_index+goods_list_len+3, key_index+goods_list_len + 3 + len(output[key]["goods_list"]) - 1, 0, 0, keys[key_index].decode("gbk"), center_style)
+		# 总额
+		sheet1.write_merge(key_index+goods_list_len+3, key_index+goods_list_len + 3 + len(output[key]["goods_list"]) - 1, 7, 7, "", center_style)
 		# 付款状态
 		sheet1.write_merge(key_index+goods_list_len+3, key_index+goods_list_len + 3 + len(output[key]["goods_list"]) - 1, 8, 8, output[key]["pay_status"].decode("gbk"), center_style)
 		# 电话
@@ -149,7 +162,7 @@ def write_output(output, output1, filename):
 		goods_list_len += len(output[key]["goods_list"]) + 3
 
 
-	header1 = ["商品名", "规格", "数量", "进货斤数", "进货价", "对外售价", "购货人", "总规格", "总数量"]
+	header1 = ["序号", "商品名", "规格", "数量", "进货斤数", "进货价", "对外售价", "购货人", "总规格", "总数量"]
 	title1 = "菜鲜省采购清单    " + filename
 	# 标题写入
 	sheet2.write_merge(0, 0, 0, 0 + len(header1)-1, title1, title_style)
@@ -157,6 +170,9 @@ def write_output(output, output1, filename):
 	# 头部写入
 	for index in range(len(header1)):
 		sheet2.write(1, index, header1[index], header_style)
+	# 头部行高
+	sheet2.row(1).height_mismatch = True
+	sheet2.row(1).height = 256 * 2
 	keys = output1.keys()
 	for key_index in range(1, len(output1)):
 		style = center_style
@@ -187,28 +203,30 @@ def write_output(output, output1, filename):
 				number_unit = number_unit_re[-1]
 
 			# 规格
-			sheet2.write(flavor_index + flavor_list_len + 2, 1, output1[key]["flavors"][flavor_index]["flavor"].decode("gbk"), style)
+			sheet2.write(flavor_index + flavor_list_len + 2, 2, output1[key]["flavors"][flavor_index]["flavor"].decode("gbk"), style)
 			# 数量
-			sheet2.write(flavor_index + flavor_list_len + 2, 2, output1[key]["flavors"][flavor_index]["number"].decode("gbk"), style)
+			sheet2.write(flavor_index + flavor_list_len + 2, 3, output1[key]["flavors"][flavor_index]["number"].decode("gbk"), style)
 			# 进货斤数
-			sheet2.write(flavor_index + flavor_list_len + 2, 3, "", style)
-			# 进货价
 			sheet2.write(flavor_index + flavor_list_len + 2, 4, "", style)
-			# 对外售价
+			# 进货价
 			sheet2.write(flavor_index + flavor_list_len + 2, 5, "", style)
+			# 对外售价
+			sheet2.write(flavor_index + flavor_list_len + 2, 6, "", style)
 			# 购货人
-			sheet2.write(flavor_index + flavor_list_len + 2, 6, output1[key]["flavors"][flavor_index]["bussiness_name"].decode("gbk"), style)
+			sheet2.write(flavor_index + flavor_list_len + 2, 7, output1[key]["flavors"][flavor_index]["bussiness_name"].decode("gbk"), style)
 
 
+		# 序号
+		sheet2.write_merge(flavor_list_len + 2, flavor_list_len + len(output1[key]["flavors"]) + 1, 0, 0, key_index, style)
 		# 商品名
-		sheet2.write_merge(flavor_list_len + 2, flavor_list_len + len(output1[key]["flavors"]) + 1, 0, 0, key.decode("gbk"), style)
+		sheet2.write_merge(flavor_list_len + 2, flavor_list_len + len(output1[key]["flavors"]) + 1, 1, 1, key.decode("gbk"), style)
 		# 总规格
-		sheet2.write_merge(flavor_list_len + 2, flavor_list_len + len(output1[key]["flavors"]) + 1, 7, 7, str(total_flavor) + flavor_unit, style)
+		sheet2.write_merge(flavor_list_len + 2, flavor_list_len + len(output1[key]["flavors"]) + 1, 8, 8, str(total_flavor) + flavor_unit, style)
 		# 数量
-		sheet2.write_merge(flavor_list_len + 2, flavor_list_len + len(output1[key]["flavors"]) + 1, 8, 8, str(total_number) + number_unit, style)
-		flavor_list_len += len(output1[key]["flavors"]) + 1
+		sheet2.write_merge(flavor_list_len + 2, flavor_list_len + len(output1[key]["flavors"]) + 1, 9, 9, str(total_number) + number_unit, style)
+		flavor_list_len += len(output1[key]["flavors"])
 
-	wb.save(os.path.join(OUTPUT, filename + "_output.xls"))
+	wb.save(os.path.join(OUTPUT, filename + "发货采购单.xls"))
 
 def useage():
 	print '''
